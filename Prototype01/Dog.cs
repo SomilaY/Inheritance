a5794c632613e52cfd0f039876d6a1ec50a0fb8d
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 12:07:58 +0000

    Add project files.

---
+++ Prototype01/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype01
+{
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The dog is eating.");
+          
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("The dog is sleeping.");
+           
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("The dog is running.");
+            Console.ReadLine();
+
+        }
+    }
+}
